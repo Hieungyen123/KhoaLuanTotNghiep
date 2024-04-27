31b8119dbd23d2979f4cbbc87af37ccd756b973c
Nguyen Hieu <100849433+Hieungyen123@users.noreply.github.com>
2024-04-27 18:27:42 +0700

    Update MongoUnil.js

--- Server/untils/MongoUnil.js
+++ Server/untils/MongoUnil.js
@@ -4,7 +4,7 @@ const MyConstants = require('./MyConstants.js');
 const uri = 'mongodb+srv://' + MyConstants.DB_USER + ':' + MyConstants.DB_PASS + '@' + MyConstants.DB_SERVER + '/' + MyConstants.DB_DATABASE;
 
 
-mongoose.connect(uri, { useNewUrlParser: true })
+mongoose.connect(uri)
   .then(() => { console.log('Connected to ' + MyConstants.DB_SERVER + '/' + MyConstants.DB_DATABASE); })
   .catch((err) => { console.error('err' + err); });
   
